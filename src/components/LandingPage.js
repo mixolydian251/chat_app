a3d92a11a7d157b2216168a3e2cdbb66db01ad93
mixolydian251 <tjhmusic11@gmail.com>
2017-12-30 22:34:14 -0500

    still lame, but slightly less so.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,14 +2,15 @@ import React from 'react';
 import io from 'socket.io-client';
 import uuid from 'uuid';
 import moment from 'moment';
+import scrollTo from '../utils/scrollAnimation';
 
 const socket = io('http://localhost:3000');
-
 
 class LandingPage extends React.Component{
 
     state = {
         endpoint: 'http://localhost:3000',
+        id: uuid(),
         username: '',
         message: '',
         messages: []
@@ -18,6 +19,7 @@ class LandingPage extends React.Component{
     send = () => {
         if( this.state.username && this.state.message ){
             socket.emit('createMessage', {
+                id: this.state.id,
                 from: this.state.username,
                 message: this.state.message
             });
@@ -47,6 +49,9 @@ class LandingPage extends React.Component{
                 messages: prevState.messages.concat(message)
             }));
 
+            const chatBox = document.getElementById("chat-box");
+
+            scrollTo(chatBox, chatBox.scrollHeight, 500);
         });
 
         socket.on('alertUserJoined', (user) => {
@@ -60,28 +65,47 @@ class LandingPage extends React.Component{
 
     render(){
         return(
-            <div>
-                <h1>The chat app of the century</h1>
-                {this.state.messages.map((message) => {
-                    return (
-                        <div>
-                            <h3>{message.from}</h3>
-                            <p>{message.message}</p>
-                            <p>{moment(Number(message.createdAt)).format('h:mm:ss')}</p>
-                        </div>
-                    )
-                })}
-                <input type="text"
-                       placeholder="username"
-                       value={this.state.username}
-                       onChange={this.handleNameChange}
-                       />
-                <textarea   type="text"
-                            placeholder="message"
-                            value={this.state.message}
-                            onChange={this.handleMessageChange}
-                />
-                    <button type="submit" onClick={this.send}>Send Message</button>
+            <div className="chat-container">
+                <h1 className="header">Worlds Best Chat App</h1>
+
+                <div id="chat-box" className="chat-box">
+                    {this.state.messages.map((message) => {
+                        return (
+                            <div className={message.id === this.state.id ?
+                                'message message__left' :
+                                'message message__right'}>
+                                <div className={message.id === this.state.id ?
+                                    'message__box message__box--left' :
+                                    'message__box message__box--right'}>
+                                    <h3 className="message__name">{message.from}</h3>
+                                    <p className="message__text">{message.message}</p>
+                                    <p className="message__date">{moment(Number(message.createdAt)).format('h:mm:ss')}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+
+
+                <div className="form">
+                        <input className="form__name"
+                               type="text"
+                               placeholder="username"
+                               value={this.state.username}
+                               onChange={this.handleNameChange}
+                        />
+
+                    <textarea className="form__text"
+                              autoFocus
+                              type="text"
+                              placeholder="message"
+                              value={this.state.message}
+                              onChange={this.handleMessageChange}
+                    />
+
+                    <button className="form__button" type="submit" onClick={this.send}>Send Message</button>
+                </div>
+
             </div>
         )
     }
